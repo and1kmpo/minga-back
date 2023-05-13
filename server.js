79121c9a20bbd52394d8a9a0f76f087be045957f
and1kmpo <acampolpez@gmail.com>
2023-05-13 01:09:07 -0500

    create server minga

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import "dotenv/config.js";
+
+const server = express(); //Crear servidor
+
+const PORT = process.env.PORT || 8080; // Establecer PORT
+
+const ready = () => { console.log('Ready on ' + PORT) };
+
+server.listen(PORT, ready); //Iniciar server
+
+console.log(process.env.NODE_ENV);
