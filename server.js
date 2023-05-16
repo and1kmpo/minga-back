a15dcbce004fb2b3f735d7cff57e4f821c183095
and1kmpo <acampolpez@gmail.com>
2023-05-15 21:08:29 -0500

    create schema and model

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ import "dotenv/config.js";
 import "./config/database.js";
 
 const server = express(); //Crear servidor
-
 const PORT = process.env.PORT || 8080; // Establecer PORT
-
 const ready = () => {
   console.log("Ready on " + PORT);
 };
 
+//Middlewares
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+//Router + Server
 server.listen(PORT, ready); //Iniciar server
 
 console.log(process.env.NODE_ENV);
