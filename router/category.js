85c3cefdfac457a9e47fd422d7273aa24452f9bf
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-06-17 00:05:56 -0500

    Category

--- router/category.js
+++ router/category.js
@@ -1,16 +1,8 @@
 import { Router } from "express";
-import Category from '../models/Category.js';
-import create from "../controllers/categories/create.js";
 import read from "../controllers/categories/read.js";
-import update from "../controllers/categories/read.js";
-import destroy from "../controllers/categories/read.js";
 
-let categoriesRouter = Router();
+const categoriesRouter = Router();
 
-categoriesRouter.post('/',create);
-categoriesRouter.get('/',read);
-categoriesRouter.put('/:id', update);
-categoriesRouter.delete('/:id', destroy);
+categoriesRouter.get("/", read);
 
 export default categoriesRouter;
-
