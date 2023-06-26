fd5b9b0f2cffced8a425c26a97b68db9f29dbd55
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-25 21:12:53 -0300

    signout

--- router/auth.js
+++ router/auth.js
@@ -1,6 +1,7 @@
 //controllers
 import register from "../controllers/auth/register.js"
 import signin from "../controllers/auth/signin.js"
+import signout from "../controllers/auth/signout.js"
 import read from "../controllers/auth/read.js"
 import update from "../controllers/auth/update.js"
 import destroy from "../controllers/auth/destroy.js"
@@ -21,6 +22,7 @@ let authRouter = Router()
 
 authRouter.post('/register', validator(register_schema), accountExistsSignUp, register)
 authRouter.post('/signin', validator(signin_schema), accountExistsSignIn, isValidPassword, signin)
+authRouter.post('/signout', accountExistsSignIn, signout)
 authRouter.get('/', read)
 authRouter.put('/:id', update)
 authRouter.delete('/:id', destroy)
