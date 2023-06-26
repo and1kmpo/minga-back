ca70c98de97fd7985c52b429c250246c60af672b
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-25 21:09:59 -0300

    signin

--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,6 @@
 //controllers
 import register from "../controllers/auth/register.js"
+import signin from "../controllers/auth/signin.js"
 import read from "../controllers/auth/read.js"
 import update from "../controllers/auth/update.js"
 import destroy from "../controllers/auth/destroy.js"
@@ -7,15 +8,19 @@ import destroy from "../controllers/auth/destroy.js"
 //middlewares
 import validator from "../middlewares/validator.js"
 import accountExistsSignUp from "../middlewares/accountExistsSignUp.js"
+import accountExistsSignIn from "../middlewares/accountExistsSignIn.js"
 
 //schemas
 import register_schema from "../schemas/auth/register.js"
+import signin_schema from "../schemas/auth/signin.js"
+import isValidPassword from "../middlewares/isValidPassword.js"
 
 //router
 import { Router } from "express"
 let authRouter = Router()
 
 authRouter.post('/register', validator(register_schema), accountExistsSignUp, register)
+authRouter.post('/signin', validator(signin_schema), accountExistsSignIn, isValidPassword, signin)
 authRouter.get('/', read)
 authRouter.put('/:id', update)
 authRouter.delete('/:id', destroy)
