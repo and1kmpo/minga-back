57643a86663f155654172e594b48451c77b61981
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-06-27 00:20:15 -0300

    M01-user (#7)
    * register
    
    * signin
    
    * signout
    
    * passport
    
    * fix
    
    * fix
    
    * fix

--- router/auth.js
+++ router/auth.js
@@ -1,15 +1,35 @@
-import { Router } from "express";
-import User from "../models/User.js";
-import create from "../controllers/auth/create.js";
-import read from "../controllers/auth/read.js";
-import update from "../controllers/auth/update.js";
-import destroy from "../controllers/auth/destroy.js";
+//controllers
+import register from "../controllers/auth/register.js"
+import signin from "../controllers/auth/signin.js"
+import signout from "../controllers/auth/signout.js"
+import token from "../controllers/auth/signin-token.js"
+import read from "../controllers/auth/read.js"
+import update from "../controllers/auth/update.js"
+import destroy from "../controllers/auth/destroy.js"
 
-let authRouter = Router();
+//middlewares
+import passport from "../middlewares/passport.js"
+import validator from "../middlewares/validator.js"
+import accountExistsSignUp from "../middlewares/accountExistsSignUp.js"
+import createHash from "../middlewares/createHash.js"
+import accountExistsSignIn from "../middlewares/accountExistsSignIn.js"
+import isValidPassword from "../middlewares/isValidPassword.js"
+import generateToken from "../middlewares/generateToken.js"
 
-authRouter.post('/', create);
-authRouter.get('/', read);
-authRouter.put('/:id', update);
-authRouter.delete('/:id', destroy);
+//schemas
+import register_schema from "../schemas/auth/register.js"
+import signin_schema from "../schemas/auth/signin.js"
 
-export default authRouter;
+//router
+import { Router } from "express"
+let authRouter = Router()
+
+authRouter.post('/register', validator(register_schema), accountExistsSignUp, createHash, register)
+authRouter.post('/signin', validator(signin_schema), accountExistsSignIn, isValidPassword, generateToken, signin)
+authRouter.post('/signout', passport.authenticate('jwt',{ session:false }), signout)
+authRouter.post('/token', passport.authenticate('jwt',{ session:false }), generateToken, token)
+authRouter.get('/', read)
+authRouter.put('/:id', update)
+authRouter.delete('/:id', destroy)
+
+export default authRouter
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import "./config/database.js";
 import indexRouter from "./router/index.js";
 import cors from "cors";
 import morgan from "morgan";
+import error_handler from "./middlewares/error_handler.js";
 
 const server = express(); //Crear servidor
 const PORT = process.env.PORT || 8080; // Establecer PORT
@@ -19,6 +20,7 @@ server.use(morgan('dev'))                               ////para registrar petic
 
 //Router + Server
 server.use('/api',indexRouter);
+server.use(error_handler)
 server.listen(PORT, ready); //Iniciar server
 
 console.log(process.env.NODE_ENV);
