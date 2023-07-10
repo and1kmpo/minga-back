704b8d55f52da65fdd49cb17cd9cb5c5ec7ef8e0
stivosp <stiven1432@gmail.com>
2023-07-10 17:04:27 -0500

    Authors

--- router/authors.js
+++ router/authors.js
@@ -2,15 +2,18 @@ import { Router } from "express";
 
 import create from "../controllers/authors/create.js";
 import read from "../controllers/authors/read.js";
-import update from "../controllers/authors/update.js";
-import destroy from "../controllers/authors/destroy.js";
+//import update from "../controllers/authors/update.js";
+//import destroy from "../controllers/authors/destroy.js";
+import validator from '../middlewares/validator.js';
+import schema_create from '../schemas/authors/create.js';
+import passport from "passport"
 
 
 let authorsRouter = Router();
 
-authorsRouter.post('/',create);
-authorsRouter.get('/',read);
-authorsRouter.put('/:id', update);
-authorsRouter.delete('/:id', destroy);
+//authorsRouter.put('/:id', update);
+//authorsRouter.delete('/:id', destroy);
+authorsRouter.post('/',passport.authenticate('jwt',{ session:false }),validator(schema_create),create)
+authorsRouter.get('/',read)
 
 export default authorsRouter;
