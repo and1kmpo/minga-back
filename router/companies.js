b70db66f96e7accbc9078153f5a8f1021eb36d3d
and1kmpo <acampolpez@gmail.com>
2023-07-13 23:23:36 -0500

    Finish model Company

--- router/companies.js
+++ router/companies.js
@@ -2,12 +2,19 @@ import { Router } from "express";
 import Company from '../models/Company.js';
 import create from "../controllers/companies/create.js";
 import read from "../controllers/companies/read.js";
-import update from "../controllers/companies/read.js";
-import destroy from "../controllers/companies/read.js";
+import update from "../controllers/companies/update.js";
+import destroy from "../controllers/companies/destroy.js";
+import validator from "../middlewares/validator.js";
+import schema_create from "../schemas/companies/companies.js";
+import passport from "passport";
+
 
 let companiesRouter = Router();
 
-companiesRouter.post('/',create);
+companiesRouter.post('/',
+passport.authenticate('jwt',{ session:false }),
+validator(schema_create),
+create);
 companiesRouter.get('/',read);
 companiesRouter.put('/:id', update);
 companiesRouter.delete('/:id', destroy);
