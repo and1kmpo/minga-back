bb4d50643119dfcaa9fee0fa32bcb9d379c3a07d
and1kmpo <acampolpez@gmail.com>
2023-07-17 18:48:26 -0500

    Add read.js file and passport implementation

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -1,10 +1,21 @@
 import Manga from "../../models/Manga.js";
 
 
-export default async (req, res) => {
+export default async (req, res, next) => {
     try {
-        let all = await Manga.find()
-        if (all) {
+        let ordenamiento = {title:1};//1 -> Ordenar ascendentemente y -1 -> Ordena descendentemente
+        let consultas = {};
+        if (req.query.title){
+            consultas.title = RegExp(req.query.title,"i");
+        }
+        if (req.query.description){
+            consultas.description = RegExp(req.query.description,"i");
+        }
+        if (req.query.sort){
+            ordenamiento.title = req.query.sort;
+        }
+        let all = await Manga.find(consultas, "title -_id description").sort(ordenamiento);
+        if (all.length > 0) {
             return res.status(200).json({ 
                 response: all,
                 message: 'Manga found successfully!'
@@ -12,7 +23,7 @@ export default async (req, res) => {
     }else{
         return res.status(404).json({
             response: null,
-            message: 'No Manga found!'
+            message: 'Manga not found!'
         })
     }
     } catch (error) {
--- router/mangas.js
+++ router/mangas.js
@@ -4,11 +4,12 @@ import create from "../controllers/mangas/create.js";
 import read from "../controllers/mangas/read.js";
 import update from "../controllers/mangas/read.js";
 import destroy from "../controllers/mangas/read.js";
+import passport from "../middlewares/passport.js";
 
 let mangasRouter = Router();
 
 mangasRouter.post('/',create);
-mangasRouter.get('/',read);
+mangasRouter.get('/',passport.authenticate('jwt',{session:false}),read);
 mangasRouter.put('/:id', update);
 mangasRouter.delete('/:id', destroy);
 
