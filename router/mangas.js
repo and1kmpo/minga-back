5fc473392e51b3b84597b49bf5702fcb65ba6298
stivosp <stiven1432@gmail.com>
2023-07-16 23:25:34 -0500

    read_one

--- router/mangas.js
+++ router/mangas.js
@@ -4,7 +4,8 @@ import create from "../controllers/mangas/create.js";
 import read from "../controllers/mangas/read.js";
 // import update from "../controllers/mangas/read.js";
 // import destroy from "../controllers/mangas/read.js";
-import passport from "passport"
+import read_one from '../controllers/mangas/read_one.js';
+import passport from "passport";
 
 
 let mangasRouter = Router();
@@ -13,5 +14,6 @@ mangasRouter.post('/',passport.authenticate('jwt',{ session:false }),create);
 mangasRouter.get('/',read);
 // mangasRouter.put('/:id', update);
 // mangasRouter.delete('/:id', destroy);
+mangasRouter.get('/:id',passport.authenticate('jwt',{ session:false }),read_one)
 
 export default mangasRouter;
