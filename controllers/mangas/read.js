844ae26b643555f97ba05214fb726fa3e1ea9fff
Natalia001-bz <125935088+Natalia001-bz@users.noreply.github.com>
2023-07-16 22:13:52 -0500

    filtros

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -1,24 +1,44 @@
 import Manga from "../../models/Manga.js";
 
 
-export default async (req, res) => {
+export default async (req, res, next) => {
     try {
-        let all = await Manga.find()
-        if (all) {
-            return res.status(200).json({ 
-                response: all,
-                message: 'Manga found successfully!'
-        })
-    }else{
-        return res.status(404).json({
-            response: null,
-            message: 'No Manga found!'
-        })
-    }
+        let consultas = {};
+        let pagination = { page: 1, limit: 4 };
+        if (req.query.page) {
+            pagination.page = req.query.page
+        };
+        if (req.query.title) {
+            consultas.title = new RegExp(req.query.title.trim(), "i")
+        };
+        if (req.query.category_id) {
+            consultas.category_id = req.query.category_id.split(',')
+            .map(category_id => category_id.trim())
+        };
+        let skip = (pagination.page > 0) ? (pagination.page - 1) * pagination.limit : 0
+        let limit = pagination.limit > 0 ? pagination.limit : 0
+        let one = await Manga.find(consultas, "title cover_photo category_id")
+            .skip(skip).limit(limit).sort({ title: 1 }).populate("category_id", "name")
+
+        if (one.length>0) {
+            let total = await Manga.countDocuments(consultas);
+            let pages = Math.ceil(total / pagination.limit);
+            let prev_page = Number (pagination.page) === 1 ? null :Number(pagination.page) - 1
+            let next_page = Number(pagination.page) === pages ? null : Number(pagination.page) + 1
+            return res.status(200).json({
+                response: one,
+                message: 'Manga found successfully!',
+                prev_page,
+                next_page
+            })
+        } else {
+            return res.status(404).json({
+                response: null,
+                message: ' Mangas not found!'
+            })
+        }
     } catch (error) {
-        return res.status(500).json({
-            response: null,
-            message: "Failed to find Manga "
-        })
+       
+    next(error)
     }
 }
