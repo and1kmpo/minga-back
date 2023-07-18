48146486cfa17616824c4136e231ea99cd57c379
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-07-18 18:34:17 -0500

    updating mangas read_one

--- controllers/mangas/read_one.js
+++ controllers/mangas/read_one.js
@@ -1,17 +1,27 @@
 import Manga from '../../models/Manga.js'
 
-
-
 export default async (req, res, next) => {
     try {
         let id = req.params.id
 
-        let one = await Manga.findById(id)
-        return res.status(200).json({
-            success: true,
-            response: one,
-            message: "Manga is Found! ",
-        })
+        let one = await Manga
+            .findById(id, "-_id -createdAt -updatedAt")
+            .populate("author_id", "-_id name last_name photo")
+            .populate("company_id", "-_id name website photo")
+            .populate("category_id", "-_id name")
+
+        if (one) {
+            return res.status(200).json({
+                success: true,
+                response: one,
+                message: "Manga is Found! ",
+            })
+        } else {
+            return res.status(404).json({
+                response: null,
+                message: 'Manga not found'
+            })
+        }
 
     }
     catch (error) {
