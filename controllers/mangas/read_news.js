a77b81ed55484727236d724c9e5922ccb49ceb17
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-07-18 00:30:18 -0500

    m09-endpoint
    Implementación endpoint
    
    GET: api/authors/me
    GET: api/mangas/news
    
    Nota: Middleware error_handler.js queda comentado dado que daba un error, queda pendiente preguntarle a los tutores.

---
+++ controllers/mangas/read_news.js
@@ -0,0 +1,61 @@
+import Manga from "../../models/Manga.js";
+
+export default async (req, res, next) => {
+  try {
+    let author = req.author_id;
+    let mangasAuthor = await Manga.find({
+      author_id: author,
+    }).populate("author_id");
+    console.log("mangas:", mangasAuthor.length);
+
+    if (mangasAuthor.length < 4) {
+      let logo =
+        "https://www.figma.com/file/odCgVyO7Jniaoft8aFcFBj/Minga---Design?type=design&node-id=2632-3722&mode=design&t=anK4ksvDR2ljr48m-4";
+      return res.status(200).json({
+        success: true,
+        response: { logo },
+        message: "Mangas is Found! ",
+      });
+    } else if (mangasAuthor.length < 8) {
+      let news = await Manga.find({
+        author_id: author,
+      })
+        .sort({ createdAt: 1 })
+        .limit(2);
+
+      let olds = await Manga.find({
+        author_id: author,
+      })
+        .sort({ createdAt: -1 })
+        .limit(2);
+
+      let all = [news, olds];
+
+      return res.status(200).json({
+        success: true,
+        response: { all },
+        message: "Mangas is Found! ",
+      });
+    } else if (mangasAuthor.length > 8) {
+      let news = await Manga.find({
+        author_id: author,
+      })
+        .sort({ createdAt: 1 })
+        .limit(4);
+
+      let old = await Manga.find({
+        author_id: author,
+      })
+        .sort({ createdAt: -1 })
+        .limit(4);
+
+      return res.status(200).json({
+        success: true,
+        response: { news, old },
+        message: "Mangas is Found! ",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
--- router/mangas.js
+++ router/mangas.js
@@ -1,18 +1,35 @@
 import { Router } from "express";
 import create from "../controllers/mangas/create.js";
 import read from "../controllers/mangas/read.js";
-import read_one from '../controllers/mangas/read_one.js';
+import read_one from "../controllers/mangas/read_one.js";
 import update from "../controllers/mangas/read.js";
 import destroy from "../controllers/mangas/read.js";
 import passport from "../middlewares/passport.js";
 
+// M09
+import read_news from "../controllers/mangas/read_news.js";
+import has_permition from "../middlewares/has_permition.js";
 let mangasRouter = Router();
 
-mangasRouter.post('/',passport.authenticate('jwt',{ session:false }),create);
-mangasRouter.get('/',passport.authenticate('jwt',{session:false}),read);
-mangasRouter.get('/:id',passport.authenticate('jwt',{ session:false }),read_one)
-mangasRouter.put('/:id', update);
-mangasRouter.delete('/:id', destroy);
+mangasRouter.get(
+  "/news",
+  passport.authenticate("jwt", { session: false }),
+  has_permition,
+  read_news
+);
 
+mangasRouter.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  create
+);
+mangasRouter.get("/", passport.authenticate("jwt", { session: false }), read);
+mangasRouter.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  read_one
+);
+mangasRouter.put("/:id", update);
+mangasRouter.delete("/:id", destroy);
 
 export default mangasRouter;
