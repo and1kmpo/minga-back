0602950702370a43ab81ac44bacda4f8ae6f4f52
Andy Campo <acampolpez@gmail.com>
2023-07-17 22:19:24 -0500

    M 05 endpoint filters (#19)
    * Model and data script companies
    
    * endpoint + controller categories
    
    * Finish model Company

--- controllers/authors/read.js
+++ controllers/authors/read.js
@@ -1,34 +1,35 @@
 import Author from "../../models/Author.js";
 
 export default async (req, res, next) => {
-    try {
-        let consultas = {};
-        let ordenamiento = { name: 1 };
-        if (req.query.city) {
-            consultas.city = new RegExp(req.query.city, "i")
-        };
-        if (req.query.name) {
-            consultas.name = new RegExp(req.query.name, "i");
-        };
+  try {
+    let consultas = {};
+    let ordenamiento = { name: 1 };
+    if (req.query.city) {
+      consultas.city = new RegExp(req.query.city, "i");
+    }
+    if (req.query.name) {
+      consultas.name = new RegExp(req.query.name, "i");
+    }
 
-if (req.query.sort){
-    ordenamiento.name = req.query.sort
-};
- let one = await Author.find(consultas, "name= id city" ).populate("user_id", "email= id photo role").sort(ordenamiento);
+    if (req.query.sort) {
+      ordenamiento.name = req.query.sort;
+    }
+    let one = await Author.find(consultas, "name= id city")
+      .populate("user_id", "email= id photo role")
+      .sort(ordenamiento);
 
- if (one.length>0){
-    return res.status(200).json({
+    if (one.length > 0) {
+      return res.status(200).json({
         response: one,
         message: "Authors found!",
-    })
- }else{
-    return res.status(404).json({
-             response: null,
-            message: 'Authors not found!'
- })
- }
-    } catch (error) {
-        next(error)
+      });
+    } else {
+      return res.status(404).json({
+        response: null,
+        message: "Authors not found!",
+      });
     }
-}
-
+  } catch (error) {
+    next(error);
+  }
+};
