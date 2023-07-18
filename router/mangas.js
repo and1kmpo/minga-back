8ce0cd998c3cfcb634a87e361a92e0df208cd2af
Andy Campo <acampolpez@gmail.com>
2023-07-17 23:22:00 -0500

    M 05 endpoint filters (#20)
    * Model and data script companies
    
    * endpoint + controller categories
    
    * Finish model Company
    
    * m-05 endpoint finished

--- router/mangas.js
+++ router/mangas.js
@@ -1,21 +1,32 @@
 import { Router } from "express";
 import create from "../controllers/mangas/create.js";
-
-import update from "../controllers/mangas/read.js";
-import destroy from "../controllers/mangas/read.js";
-import passport from "passport"
+import read from "../controllers/mangas/read.js";
+import update from "../controllers/mangas/update.js";
+import destroy from "../controllers/auth/destroy.js";
+import passport from "passport";
 import schema from "../schemas/mangas/create.js";
 import has_permition from "../middlewares/has_permition.js";
 import validator from "../middlewares/validator.js";
 import isActive from "../middlewares/isActive.js";
-import read_one from '../controllers/mangas/read_one.js';
+import read_one from "../controllers/mangas/read_one.js";
 
 let mangasRouter = Router();
 
-mangasRouter.post('/',passport.authenticate('jwt',{ session:false }),validator(schema), has_permition, isActive, create);
-mangasRouter.get('/',passport.authenticate('jwt',{session:false}),read);
-mangasRouter.get('/:id',passport.authenticate('jwt',{ session:false }),read_one)
-mangasRouter.put('/:id', update);
-mangasRouter.delete('/:id', destroy);
+mangasRouter.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  validator(schema),
+  has_permition,
+  isActive,
+  create
+);
+mangasRouter.get("/", passport.authenticate("jwt", { session: false }), read);
+mangasRouter.get(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  read_one
+);
+mangasRouter.put("/:id", update);
+mangasRouter.delete("/:id", destroy);
 
 export default mangasRouter;
