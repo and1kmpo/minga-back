3f60b096f790e62f231afc8f029d8da052e7c342
Natalia001-bz <125935088+Natalia001-bz@users.noreply.github.com>
2023-07-17 19:39:36 -0500

    schema manga

--- router/mangas.js
+++ router/mangas.js
@@ -5,11 +5,21 @@ import read from "../controllers/mangas/read.js";
 // import update from "../controllers/mangas/read.js";
 // import destroy from "../controllers/mangas/read.js";
 import passport from "passport"
+import schema from "../schemas/mangas/create.js";
+import has_permition from "../middlewares/has_permition.js";
+import validator from "../middlewares/validator.js";
+import isActive from "../middlewares/isActive.js";
+
+
 
 
 let mangasRouter = Router();
 
-mangasRouter.post('/',passport.authenticate('jwt',{ session:false }),create);
+mangasRouter.post('/',passport.authenticate('jwt',{ session:false }),
+validator(schema),
+ has_permition,
+ isActive,
+  create);
 mangasRouter.get('/',read);
 // mangasRouter.put('/:id', update);
 // mangasRouter.delete('/:id', destroy);
