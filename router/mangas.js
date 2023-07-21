b8147a3c71f84f505356362633460d30cd5c473e
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-07-21 11:36:25 -0500

    Fix identation in routers and ordering imports (#24)
    * Fix identation and order imports for router mangas
    
    * Fix order imports for router chapters
    
    * Fix identation for router auth
    
    * Fix identation and order imports for router authors
    
    * Fix model company to default active false

--- router/mangas.js
+++ router/mangas.js
@@ -1,19 +1,21 @@
 import { Router } from "express";
+// controllers
 import create from "../controllers/mangas/create.js";
 import read from "../controllers/mangas/read.js";
 import read_one from "../controllers/mangas/read_one.js";
-import passport from "../middlewares/passport.js";
 import update from "../controllers/mangas/update.js";
-import destroy from "../controllers/auth/destroy.js";
-import schema from "../schemas/mangas/create.js";
+import read_news from "../controllers/mangas/read_news.js";
+import destroy from "../controllers/mangas/destroy.js";
+// middlewares
+import passport from "../middlewares/passport.js";
 import validator from "../middlewares/validator.js";
 import isActive from "../middlewares/isActive.js";
-
-// M09
-import read_news from "../controllers/mangas/read_news.js";
 import has_permition from "../middlewares/has_permition.js";
-let mangasRouter = Router();
+// schemas
+import schema from "../schemas/mangas/create.js";
 
+
+let mangasRouter = Router();
 
 mangasRouter.get( "/news",
   passport.authenticate("jwt", { session: false }),
@@ -30,6 +32,6 @@ mangasRouter.post("/",
 mangasRouter.get("/", passport.authenticate("jwt", { session: false }), read);
 mangasRouter.get("/:id", passport.authenticate("jwt", { session: false }), read_one);
 mangasRouter.put("/:id", update);
-mangasRouter.delete("/:id", destroy);
+// mangasRouter.delete("/:id", destroy);
 
 export default mangasRouter;
