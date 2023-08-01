6e92b819fb1a6239e84eea11abf1c484279a9564
Robert Michael González Núñez <71914244+ROBERTG96@users.noreply.github.com>
2023-08-01 03:09:37 -0500

    read_news

--- controllers/mangas/read_news.js
+++ controllers/mangas/read_news.js
@@ -2,51 +2,49 @@ import Manga from "../../models/Manga.js";
 
 export default async (req, res, next) => {
   try {
-    let author = req.author_id;
-    let mangasAuthor = await Manga.find({
+    let author = req.author._id;
+    console.log("AUTHOR DE MANGAS:", author);
+    const mangasAuthor = await Manga.find({
       author_id: author,
-    }).populate("author_id");
+    });
+
     console.log("mangas:", mangasAuthor.length);
 
-    if (mangasAuthor.length < 4) {
-      let logo =
-        "https://www.figma.com/file/odCgVyO7Jniaoft8aFcFBj/Minga---Design?type=design&node-id=2632-3722&mode=design&t=anK4ksvDR2ljr48m-4";
-      return res.status(200).json({
-        success: true,
-        response: { logo },
-        message: "Mangas is Found! ",
-      });
-    } else if (mangasAuthor.length < 8) {
+    if (mangasAuthor.length < 8 && mangasAuthor.length > 4) {
       let news = await Manga.find({
-        author_id: author,
-      })
-        .sort({ createdAt: 1 })
-        .limit(2);
-
-      let olds = await Manga.find({
         author_id: author,
       })
         .sort({ createdAt: -1 })
         .limit(2);
 
+      let olds = await Manga.find({
+        author_id: author,
+      })
+        .sort({ createdAt: 1 })
+        .limit(2);
+
       let all = [news, olds];
+      console.log("aca fue 1");
 
       return res.status(200).json({
         success: true,
         response: { all },
         message: "Mangas is Found! ",
       });
+
+      console.log("====================================");
+      console.log("====================================");
     } else if (mangasAuthor.length > 8) {
       let news = await Manga.find({
         author_id: author,
       })
-        .sort({ createdAt: 1 })
+        .sort({ createdAt: -1 })
         .limit(4);
 
       let old = await Manga.find({
         author_id: author,
       })
-        .sort({ createdAt: -1 })
+        .sort({ createdAt: 1 })
         .limit(4);
 
       return res.status(200).json({
@@ -54,8 +52,19 @@ export default async (req, res, next) => {
         response: { news, old },
         message: "Mangas is Found! ",
       });
+    } else {
+      let logo =
+        "https://www.figma.com/file/odCgVyO7Jniaoft8aFcFBj/Minga---Design?type=design&node-id=2632-3722&mode=design&t=anK4ksvDR2ljr48m-4";
+      return res.status(200).json({
+        success: true,
+        response: { logo },
+        message: "Mangas is Found! ",
+      });
     }
   } catch (error) {
+    console.log("====================================");
+    console.log(error);
+    console.log("====================================");
     next(error);
   }
 };
