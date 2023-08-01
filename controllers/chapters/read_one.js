41c1eb15433f200571df3233cd575b90f49cba87
Natalia001-bz <125935088+Natalia001-bz@users.noreply.github.com>
2023-08-01 00:50:13 -0500

    configuración endpoint M08

--- controllers/chapters/read_one.js
+++ controllers/chapters/read_one.js
@@ -9,11 +9,16 @@ export default async (req, res, next) => {
         let one = await Chapter.findById(chapter_id, "-createdAt -updatedAt")
         let next= await Chapter.findOne(
             {_id: { $gt: chapter_id } }
-        )
+        ).sort({ _id: 1 })
+        .limit(1)
+        let prev = await Chapter.findOne({ _id: { $lt: chapter_id } }, "-createdAt -updatedAt")
+      .sort({ _id: -1 })
+      .limit(1);
         return res.status(200).json({
             success: true,
             response: one,
             next: next ? next._id : null,
+            prev: prev ? prev._id : null,
             message: "Chapter is Found! "
             
         })
