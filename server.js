36f9f0bea8072c8e418d07abea75dd81df252540
and1kmpo <acampolpez@gmail.com>
2023-08-05 00:40:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,19 +8,19 @@ import error_handler from "./middlewares/error_handler.js";
 import not_found_handler from "./middlewares/not_found_handler.js";
 
 const server = express(); //Crear servidor
-const PORT = process.env.PORT || 8080; // Establecer PORT
+const PORT = process.env.PORT || 8000; // Establecer PORT
 const ready = () => {
   console.log("Ready on " + PORT);
 };
 
 //Middlewares
-server.use(express.json());//permite entradas y también trabajar con formato json
-server.use(express.urlencoded({ extended: true }));//permite capturar consultas complejas
-server.use(cors())                                      //para permitir orígenes cruzados (front/back)
-server.use(morgan('dev'))                               ////para registrar peticiones HTTP
+server.use(express.json()); //permite entradas y también trabajar con formato json
+server.use(express.urlencoded({ extended: true })); //permite capturar consultas complejas
+server.use(cors()); //para permitir orígenes cruzados (front/back)
+server.use(morgan("dev")); ////para registrar peticiones HTTP
 
 //Router + Server
-server.use('/api',indexRouter);
+server.use("/api", indexRouter);
 server.use(not_found_handler);
 server.use(error_handler);
 server.listen(PORT, ready); //Iniciar server
