8cab0a52e008194048540ed080d04aabd8597254
EstefaniaMarin <125941129+EstefaniaMarin@users.noreply.github.com>
2023-08-28 12:41:28 -0500

    endpoint M_14 is ok

---
+++ router/reactions.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import create from "../controllers/reactions/create.js";
+import passport from "../middlewares/passport.js";
+import read from "../controllers/reactions/read.js";
+
+const reactionsRouter = Router();
+
+reactionsRouter.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  create
+);
+reactionsRouter.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  read
+);
+
+export default reactionsRouter;
